64d73228caf54990694317efa526ab35e8cd03ba
Weronika <weronikaskowron1@gmail.com>
2026-04-03 16:01:30 +0200

    Drewno wypada i można je zebrać

--- Scripts/tree.gd
+++ Scripts/tree.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 var player_in_range := false
-var hits_needed := 4
+var hits_needed := 4 #ilosc potrzebnych uderzen
 var hits := 0
+
+@export var wood_scene: PackedScene #instancja sceny drewna
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +18,17 @@ func _process(delta: float) -> void:
 
 		if hits >= hits_needed: #jesli player przekroczy ilosc uderzen
 			print("drzewo zniszczone")
-			queue_free()
+			queue_free() #drzewo znika
+			drop_wood()
+			
+			
+			
+			
+func drop_wood():
+	if wood_scene:
+		var wood = wood_scene.instantiate()
+		get_parent().add_child(wood)
+		wood.global_position = global_position + Vector2(0, 20)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:

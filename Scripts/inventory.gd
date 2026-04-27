5d0b06776b32a0a9874844c1121fa086a7b39d74
Weronika <weronikaskowron1@gmail.com>
2026-04-27 02:48:05 +0200

    zaznaczanie w ekwipunku

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var selected_slot = null
+
 @onready var grid: GridContainer = $Panel/GridContainer
 var slot_scene = preload("res://Scenes/item_slot.tscn") #sciezka do item slot
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +13,9 @@ func _ready() -> void:
 func _process(delta: float) -> void: #pojawianie się ewkipunku na E
 	if Input.is_action_just_pressed("inventory"):
 		visible = !visible
+		if not visible:
+			clear_selection() #zaznaczenie odklikuje sie po wyłączeniu ekwipunka
+
 
 func refresh(player_inventory: Array) -> void:
 	for child in grid.get_children():
@@ -20,3 +25,20 @@ func refresh(player_inventory: Array) -> void:
 		var slot = slot_scene.instantiate()
 		grid.add_child(slot)
 		slot.set_item(item_data["texture"], item_data["amount"])
+		slot.slot_clicked.connect(_on_slot_clicked)
+
+func _on_slot_clicked(slot):
+	if selected_slot == slot: #jeśli ten sam to odznaczamy
+		slot.set_selected(false)
+		selected_slot = null
+	else:
+		if selected_slot != null: #jeśli inny to go odznacz
+			selected_slot.set_selected(false)
+
+		selected_slot = slot #zaznaczamy nowy
+		selected_slot.set_selected(true)
+		
+func clear_selection():
+	if selected_slot != null:
+		selected_slot.set_selected(false)
+		selected_slot = null
--- Scripts/item_slot.gd
+++ Scripts/item_slot.gd
@@ -1,10 +1,13 @@
 extends Panel
 
+signal slot_clicked(slot)
+
+var selected := false
 @onready var icon: TextureRect = $TextureRect
 @onready var amount_label: Label = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	mouse_filter = Control.MOUSE_FILTER_STOP
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +19,21 @@ func set_item(texture: Texture2D, amount: int) -> void: #ustawia teksturke i ilo
 	amount_label.text = str(amount)
 	#icon.position = Vector2(38, 58) + (Vector2(64, 64) - icon.size) / 2 - Vector2(7, 7)
 	icon.position = Vector2(7, 7)
+	
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			slot_clicked.emit(self)
+
+func set_selected(value: bool) -> void:
+	selected = value
+
+	if selected: #rysowanie obramowania
+		var style := StyleBoxFlat.new()
+		style.bg_color = Color(0.0, 0.0, 0.0, 0.196)
+		style.border_color = Color.BLACK
+		style.set_border_width_all(5)
+		add_theme_stylebox_override("panel", style)
+	else:
+		var style_clear = StyleBoxEmpty.new()
+		add_theme_stylebox_override("panel", style_clear)

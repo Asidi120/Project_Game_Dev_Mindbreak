1ea9174cfda7609ae68f90bce00185a729e956d8
Weronika <weronikaskowron1@gmail.com>
2026-06-23 01:16:39 +0200

    poprawa func eating

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -235,31 +235,43 @@ func update_held_position():
 
 func eating(item):
 	var czy_zjedzone = false
-	if not inventory_system.inventory_visible:
-		if item["item_id"] == "potion_health": #health potka
-			if current_hp == 200:
-				return
-			else:
-				current_hp = 200
-				emit_signal("hp_changed", current_hp, max_hp)
-		if item["item_id"] == "potion_stamina": #wypicie stamina potion
-			drank_stamina_potion = true
-			stamina_bar.modulate = Color(0.0, 0.853, 0.0, 1.0)
-			
-			print("heath potka")
-		elif item is Food:
-			if item["hunger_points"] <= max_hunger:
-				if current_hunger == max_hunger:
-					print("Nie można zjeść. Jesteś najedzony!")
-					return
-				elif item["hunger_points"] + current_hunger >= max_hunger:
-					current_hunger = max_hunger
-					print("Najadłeś się")
-				else:
-					print("Zjadłeś")
-					current_hunger += item["hunger_points"]
+	
+	if inventory_system.inventory_visible:
+		return false
+	
+	if item == null:
+		return false
+	
+	if item["item_id"] == "potion_health":
+		if current_hp == 200:
+			return false
+		
+		current_hp = 200
+		emit_signal("hp_changed", current_hp, max_hp)
 		czy_zjedzone = true
-		return czy_zjedzone
+	
+	elif item["item_id"] == "potion_stamina":
+		drank_stamina_potion = true
+		stamina_bar.modulate = Color(0.0, 0.853, 0.0, 1.0)
+		czy_zjedzone = true
+	
+	elif item.has("hunger_points"):
+		if current_hunger == max_hunger:
+			print("Nie można zjeść. Jesteś najedzony!")
+			return false
+		
+		current_hunger += item["hunger_points"]
+		
+		if current_hunger >= max_hunger:
+			current_hunger = max_hunger
+			print("Najadłeś się")
+		else:
+			print("Zjadłeś")
+		
+		emit_signal("hunger_changed", current_hunger, max_hunger)
+		czy_zjedzone = true
+	
+	return czy_zjedzone
 
 
 func throw():
